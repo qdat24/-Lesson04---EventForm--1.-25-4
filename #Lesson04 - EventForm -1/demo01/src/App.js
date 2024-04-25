9eec2f45af63cf0dbfb6f928a8a4627972a49c8c
qdat24 <166087439+qdat24@users.noreply.github.com>
2024-04-25 10:50:57 +0700

    Update App.js

--- #Lesson04 - EventForm -1/demo01/src/App.js
+++ #Lesson04 - EventForm -1/demo01/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
-import VDK_EventForm1 from './components/VDK_EventForm1';
-import VDK_EventForm2 from './components/VDK_EventForm2';
-import VDK_EventForm3 from './components/VDK_EventForm3';
-import VDK_EventForm4 from './components/VDK_EventForm4';
+import DQD_EventForm1 from './components/DQD_EventForm1';
+import DQD_EventForm2 from './components/DQD_EventForm2';
+import DQD_EventForm3 from './components/DQD_EventForm3';
+import DQD_EventForm4 from './components/DQD_EventForm4';
 
 export default class App extends Component {
   render() {
     return (
       <div className='container'>
         <h1>Event Form Demo </h1>
-        <VDK_EventForm1 />
-        <VDK_EventForm2 />
-        <VDK_EventForm3 />
-        <VDK_EventForm4 name="vu duc khoa" />
+        <DQD_EventForm1 />
+        <DQD_EventForm2 />
+        <DQD_EventForm3 />
+        <DQD_EventForm4 name="dinh quoc dat" />
       </div>
     );
   }
